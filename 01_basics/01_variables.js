add64118c86ebd7268a85311ccc239c03c96b538
AMAN PAGDA <alishpagda08@gmail.com>
2025-04-24 17:16:06 +0530

    Datatypes done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144455
+var accountEmail = "amanpagda20@gmail.com"
+let accountPassword = "123456789"
+accountCity = "jamnagar"
+
+let account
+
+// accountId = 2 // Never Change value of Const.
+
+accountEmail = "alishpagda08@gmail.com"
+accountPassword = "pagda2636"
+accountCity = "Rajkot"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, account])
