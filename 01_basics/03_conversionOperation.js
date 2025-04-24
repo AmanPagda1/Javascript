76701fdff8fc22704cd194cd8a7e74d3f67f36d6
AMAN PAGDA <alishpagda08@gmail.com>
2025-04-24 19:28:34 +0530

    done one

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,63 @@
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"  
+
+
+let logedIn = ""
+
+let logedIntest = Boolean(logedIn)
+// console.log(logedIntest); // true
+
+// string
+let score = "33aman"
+
+// console.log(typeof score);
+
+let valueInNuber = Number(score)
+// console.log(valueInNuber)
+// console.log(typeof valueInNuber)
+// console.log("");
+
+                                     // Number
+let scoreone = 33
+
+// console.log(typeof scoreone);
+
+let valueInNuberone = Number(scoreone)
+// console.log(valueInNuberone)
+// console.log(typeof valueInNuberone)
+
+                                    // boolean
+let scoretwo = true
+
+// console.log(typeof scoretwo);
+
+let valueInNubertwo = Number(scoretwo)
+// console.log(valueInNubertwo)
+// console.log(typeof valueInNubertwo)
+
+                                    // null
+let scorethree = null
+
+// console.log(typeof scorethree);
+
+let valueInNumberthree = Number(scorethree)
+// console.log(valueInNumberthree);
+// console.log(typeof valueInNumberthree)
+
+                                    // undefined
+let scorefour = undefined
+
+// console.log(typeof scorefour);
+
+let valueInNuberfour = Number(scorefour)
+// console.log(valueInNuberfour)
+// console.log(typeof valueInNuberfour)
