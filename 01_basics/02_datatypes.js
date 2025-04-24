5efd725cc7e25409002724443121ea9e7e4f7280
AMAN PAGDA <alishpagda08@gmail.com>
2025-04-24 17:18:44 +0530

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,2 +1,11 @@
 console.log(typeof null); // object
 console.log(typeof undefined); // undefined
+
+// Number = 2 to power of 53
+// BigInt = 
+// bigint = ""
+// String = "aman"
+// Boolean = true/false
+// Symbol = unique value
+// null = ( null is an object )
+// undefined = ( undefined is a type of variable which is not defined yet )
